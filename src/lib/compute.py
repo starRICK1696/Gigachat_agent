48ea6b5b4631bc4655b78b2fbb2a9abed95ca917
PR0GUEE <alexavgpost@gmail.com>
2026-03-28 03:35:55 +0300

    added reurn functions

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -12,85 +12,83 @@ import numpy as np
 
 
 class BasicEvaluator:
-    def __init__(self, matrix=None):
-        self.matrix = matrix
+    def __init__(self):
+        self.evaluator = None
 
-    def evaluation_function(self, **kwargs):
-        exit("Error : evaluation function is not implemented. ")
+    def __evaluation_function(self, matrix, **kwargs):
+        raise NotImplementedError("Evaluation function is not implemented.")
 
-    def evaluate(self, display=False, num_reads=100, arglist=None):
-        if arglist is None:
-            arglist = []
+    def __return(self):
+        raise NotImplementedError("Return function is not implemented.")
 
-        if self.matrix is None:
-            exit("Error : Input data is incorrect")
-
-        eval = self.evaluation_function(arglist)
-
-        eval.find_min_energy(num_reads)
-
-        return eval.solution
+    def evaluate(self, matrix, display=False, num_reads=100, *args, **kwargs):
+        self.evaluator = self.__evaluation_function(matrix, *args, **kwargs)
+        self.evaluator.find_min_energy(num_reads)
+        return self.__return()
 
 
 class SalesmanEvaluator(BasicEvaluator):
-    def __init__(self, matrix=None, from_coordinates=False):
-        self.from_coordinates = from_coordinates
-        super().__init__(matrix)
-
-    def evaluation_function(self, **kwargs):
-        if self.from_coordinates:
-            return QUBOSalesman(coord_matrix=self.matrix)
-        return QUBOSalesman(adjacency_matrix=self.matrix)
-
+    def __evaluation_function(self, matrix, from_coordinates=False, **kwargs):
+        if from_coordinates:
+            return QUBOSalesman(coord_matrix=matrix)
+        return QUBOSalesman(adjacency_matrix=matrix)
+    
+    def __return(self):
+        if self.evaluator is None:
+            raise NotImplementedError("Evaluation function is not implemented.")
+        answer = 0
+        for i in range(len(self.evaluator.solution)):
+            answer += self.evaluator.adjacency_matrix[self.evaluator.solution[i]][self.evaluator.solution[(i + 1) % len(self.evaluator.solution)]]
+        return {"answer" : answer, "characteristics" : self.evaluator.solution}
 
 
 class CliqueEvaluator(BasicEvaluator):
-    def __init__(self, matrix=None):
-        super().__init__(matrix)
-
-    def evaluation_function(self, **kwargs):
-        return QUBOClique(self.matrix)
+    def __evaluation_function(self, matrix, **kwargs):
+        return QUBOClique(matrix)
     
+    def __return(self):
+        return {"answer" : len(self.evaluator.solution), "characteristics" : self.evaluator.solution}
+
 
 class KnapsackEvaluator(BasicEvaluator):
-    def __init__(self, matrix=None):
-        super().__init__(matrix)
-
-    def evaluation_function(self, capacity, **kwargs):
-        return QUBOKnapsack(self.matrix, capacity)
+    def __evaluation_function(self, matrix, capacity, **kwargs):
+        return QUBOKnapsack(matrix, capacity)
+    
+    def __return(self):
+        return {"answer" : self.sum_weight, "characteristics" : self.evaluator.solution}
 
 
 class Tiling2DimEvaluator(BasicEvaluator):
-    def __init__(self, matrix=None):
-        super().__init__(matrix)
-
-    def tester_function(self, width, length, **kwargs):
-        return QUBOTiling2Dim(self.matrix, width, length)
+    def __evaluation_function(self, matrix, width, length, **kwargs):
+        return QUBOTiling2Dim(matrix, width, length)
+    
+    def __return(self):
+        return {"answer" : sum([sum(row) for row in self.evaluator.solution]), "characteristics" : self.evaluator.solution}
 
 
 class ProductionSchedulingEvaluator(BasicEvaluator):
-    def __init__(self, matrix=None):
-        super().__init__(matrix)
-
-    def tester_function(self, setup_times, job_values, **kwargs):
-        return QUBOProductionScheduling(self.matrix, setup_times, job_values)
+    def __evaluation_function(self, matrix, setup_times, job_values, **kwargs):
+        return QUBOProductionScheduling(matrix, setup_times, job_values)
+    
+    def __return(self):
+        return {"answer" : "some loss", "characteristics" : self.evaluator.solution}
 
 
 class MultiKnapEvaluator(BasicEvaluator):
-    def __init__(self, matrix=None):
-        super().__init__(matrix)
-
-    def tester_function(self, capabilities, **kwargs):
-        return QUBOMultiKnapsack(self.matrix, capabilities)
+    def __evaluation_function(self, matrix, capabilities, **kwargs):
+        return QUBOMultiKnapsack(matrix, capabilities)
+    
+    def __return(self):
+        return {"answer" : -self.min_energy, "characteristics" : self.evaluator.solution}
 
 
 class MaxWeightCliqueEvaluator(BasicEvaluator):
-    def __init__(self, matrix=None):
-        super().__init__(matrix)
-
-    def tester_function(self, **kwargs): 
-        return QUBOMaxWeightClique(self.matrix)
-
-
-
-
+    def __evaluation_function(self, matrix, **kwargs):
+        return QUBOMaxWeightClique(matrix)
+    
+    def __return(self):
+        sum_edge = 0
+        for i in range(len(self.evaluator.solution)):
+            for j in range(i + 1, len(self.evaluator.solution)):
+                sum_edge += self.evaluator.adjacency_matrix[self.evaluator.solution[i]][self.evaluator.solution[j]]
+        return {"answer" : len(self.evaluator.solution), "characteristics" : self.evaluator.solution, "sum_edges" : sum_edge}
